71ff6b8d7306fd829264fa33fe9ae9ded78da558
bbk-pij-2012-23 <e@nowhere.com>
2013-03-30 12:45:10 +0000

    starting with mockito

--- Poker/src/poker/TestCalculator.java
+++ Poker/src/poker/TestCalculator.java
@@ -4,8 +4,8 @@
 package poker;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 /**
  * @author eleanormann
@@ -15,9 +15,16 @@ public class TestCalculator {
 
 	@Test
 	public void test() {
-		Calculator testCal =new Calculator();
-		assertTrue(testCal);
+		Calculator testCal = new Calculator();
+		assertTrue(testCal!=null);
 
+	}
+	
+	@Test
+	public void testCountValue(){
+		Hand mockedHand = mock(Hand.class);
+		when(mockedHand.getValue(1).thenReturn(1));
+		
 	}
 
 }
