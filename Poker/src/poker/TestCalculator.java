3b9886aede3c82d5135aae194f09c756db870944
bbk-pij-2012-23 <e@nowhere.com>
2013-03-30 11:40:39 +0000

    first commit

---
+++ Poker/src/poker/TestCalculator.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package poker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author eleanormann
+ *
+ */
+public class TestCalculator {
+
+	@Test
+	public void test() {
+		Calculator testCal =new Calculator();
+		assertTrue(testCal);
+
+	}
+
+}
---
+++ Poker/src/poker/TestHand.java
@@ -0,0 +1,14 @@
+package poker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestHand {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
